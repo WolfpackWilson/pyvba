fd083bf6425a7806c98aae1e75032f03ea8ae5ed
Jack Wilson <wilso313@wwu.edu>
2020-08-19 13:31:41 -0700

    prepared test cases and started Viewer test cases

---
+++ tests/integration/test_programming.py
@@ -0,0 +1,51 @@
+import unittest
+import pyvba
+
+
+class TestBrowser(unittest.TestCase):
+    def test_all(self):
+        pass
+
+    def test_print_browser(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def test_goto(self):
+        pass
+
+    def test_regen(self):
+        pass
+
+
+class TestXMLExport(unittest.TestCase):
+    def test_string(self):
+        pass
+
+    def test_min(self):
+        pass
+
+    def test_print(self):
+        pass
+
+    def test_save(self):
+        pass
+
+
+class TestJSONExport(unittest.TestCase):
+    def test_string(self):
+        pass
+
+    def test_min(self):
+        pass
+
+    def test_print(self):
+        pass
+
+    def test_save(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
